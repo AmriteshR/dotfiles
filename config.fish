d8fc5d5a19f89552ab0d0b2d9e754de7fc525aba
AmriteshR <raianshul1997@gmail.com>
2023-01-23 22:21:28 +0530

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,25 +1,17 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-#neofetch
-alias dev_aws='sudo ssh -i ~/Mynd/Instance_54.198.152.236.pem ubuntu@35.172.92.13'
-alias PAT='cat ~/Mynd/PAT'
-# starship init fish | source
 
-alias ll='exa -la'
+alias dev_aws='sudo ssh -i ~/Work/Instance_54.198.152.236.pem ubuntu@35.172.92.13'
+alias secur_prod='ssh -i ~/Work/Secur_Instance.pem ubuntu@54.85.243.167'
+alias PAT='cat ~/Work/PAT | xclip -selection clipboard'
+# starship init fish | source
+alias ll='exa -la --icons'
 alias gitlu='git log --graph --abbrev-commit --decorate'
 alias tree='exa --tree --level=3'
-alias update='sudo nala update && sudo nala upgrade'
 alias lg="git log --all --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'"
 
 # pyenv init
-if command -v pyenv 1>/dev/null 2>&1
-  pyenv init - | source
-end
+pyenv init - | source
 
-
-
-# pyenv init
-if command -v pyenv 1>/dev/null 2>&1
-  pyenv init - | source
-end
+starship init fish | source
